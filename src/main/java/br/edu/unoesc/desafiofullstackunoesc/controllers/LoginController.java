3d9d5eb0d6fd22ee8f393f6856158a88a2fe5238
Fernanda <fernanda@inovadora.com.br>
2022-03-27 13:26:09 -0300

    add novo template login, add cadastro.html

---
+++ src/main/java/br/edu/unoesc/desafiofullstackunoesc/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package br.edu.unoesc.desafiofullstackunoesc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "login/index";
+	}
+
+	@GetMapping("/cadastro")
+	public String cadastro() {
+		return "signup/cadastro";
+	}
+}
